36260d416085b4cc9e7ff6737d58825db2f54e5c
Harsh Thakur <90609542+harshthakur2021@users.noreply.github.com>
2025-06-24 17:37:39 +0530

    Testimonials Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from './components/Header'
 import About from './components/About'
+import Projects from './components/Projects'
+import Testimonails from './components/Testimonails'
 
 function App() {
   return (
     <div className="w-full overflow-hidden ">
       <Header />
       <About/>
+      <Projects/>
+      <Testimonails/>
     </div>
   )
 }
