67add805717be166cdadfc06d0e3d9e400c785be
Harsh Thakur <90609542+harshthakur2021@users.noreply.github.com>
2025-06-24 09:18:42 +0530

    Navbar and Header Section Completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,22 @@
-import React, { use, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
 
 function Navbar() {
   const [showMobileMenu, setShowMobileMenu] = useState(false); 
+
+  useEffect(()=>{
+    if(showMobileMenu){
+      document.body.style.overflow ='hidden'
+    }
+    else{
+      document.body.style.overflow ='auto'
+    }
+
+    return()=>{
+      document.body.style.overflow ='auto'
+    }
+
+  },[showMobileMenu])
   return (
     <div className='absolute top-0 left-0 w-full z-10'>
       <div className='container mx-0 flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
@@ -14,18 +28,18 @@ function Navbar() {
           <a href="#Header" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
         </ul>
         <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Signup</button>
-        <img src={assets.menu_icon}className='md:hidden w-7 cursor-pointer'alt="" />
+        <img  onClick={()=> setShowMobileMenu(true)}src={assets.menu_icon}className='md:hidden w-7 cursor-pointer'alt="" />
       </div>
       {/*--------Mobile-menu-------*/}
-      <div className='md:hidden fixed w-full right-0 top-0 bottom-0 overflow-hidden bg-white transition-all'>
+      <div className={`md:hidden ${showMobileMenu?'fixed w-full' : "h-0 w-0"} right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}>
         <div className='flex justify-end p-6 cursor-pointer'>
-          <img src={assets.cross_icon} className='w-6' alt="" />
+          <img onClick={()=>setShowMobileMenu(false)} src={assets.cross_icon} className='w-6' alt="" />
         </div>
         <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium '>
-          <a href="#Header" className='px-4 py-2 rounded-full inline-block'>Home</a>
-          <a href="#About" className='px-4 py-2 rounded-full inline-block'>About</a>
-          <a href="#Projects" className='px-4 py-2 rounded-full inline-block'>Projects</a>
-          <a href="#Testimonails" className='px-4 py-2 rounded-full inline-block'>Testimonails</a>
+          <a onClick={()=>setShowMobileMenu(false)} href="#Header" className='px-4 py-2 rounded-full inline-block'>Home</a>
+          <a onClick={()=>setShowMobileMenu(false)}href="#About" className='px-4 py-2 rounded-full inline-block'>About</a>
+          <a onClick={()=>setShowMobileMenu(false)}href="#Projects" className='px-4 py-2 rounded-full inline-block'>Projects</a>
+          <a onClick={()=>setShowMobileMenu(false)} href="#Testimonails" className='px-4 py-2 rounded-full inline-block'>Testimonails</a>
         </ul>
       </div>
     </div>
